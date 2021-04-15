8d84f3eb530fd6ddccc053ebebf7956ca3809cd7
ByteSized <neville.keemer@gmail.com>
2021-04-15 11:05:03 +0100

    Fixed typo in homepage test.
    Close #8

--- cypress/integration/test.js
+++ cypress/integration/test.js
@@ -1,4 +1,4 @@
-it("Has correct title", () =>  {
-    cy.visit("/");
-    cy.get("h1").contains("Movie review blog!");  
-})
+it("Has correct title", () => {
+  cy.visit("/");
+  cy.get("h1").contains("Movie Review Blog!");
+});
